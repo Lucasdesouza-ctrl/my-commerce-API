a3117d2a054052afb75e18d93ae1f96359a8845d
thiagomsantanna <thiagomsantanna11@gmail.com>
2025-08-11 23:12:56 -0300

    fix: Adding gradle task to generate RSA Keys

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.Base64
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.3"
@@ -51,4 +56,42 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.register("generateJwtKeys") {
+	group = "build"
+	description = "Generates RSA public and private keys for JWT"
+
+	doLast {
+		val keyDir = file("src/main/resources/keys").apply { mkdirs() }
+
+		// Generate RSA Key Pair (2048-bit)
+		val keyPairGenerator = KeyPairGenerator.getInstance("RSA").apply {
+			initialize(2048)
+		}
+		val keyPair = keyPairGenerator.generateKeyPair()
+		val publicKey = keyPair.public as RSAPublicKey
+		val privateKey = keyPair.private as RSAPrivateKey
+
+		// Write keys in PEM format
+		fun encodeKeyToPem(key: ByteArray, type: String): String {
+			val base64Key = Base64.getEncoder().encodeToString(key)
+			val key = base64Key.chunked(64).joinToString("\n");
+
+			return "-----BEGIN $type KEY-----\n${key}\n-----END $type KEY-----"
+		}
+
+		val publicPem = encodeKeyToPem(publicKey.encoded, "PUBLIC")
+		val privatePem = encodeKeyToPem(privateKey.encoded, "PRIVATE")
+
+		// Save keys to files
+		keyDir.resolve("public.pub").writeText(publicPem)
+		keyDir.resolve("private.key").writeText(privatePem)
+
+		println("\uD83D\uDD11 Generated JWT keys in ${keyDir.path}")
+	}
+}
+
+tasks.named("bootJar") {
+	dependsOn("generateJwtKeys")
 }
